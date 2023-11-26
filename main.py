0e67298af529ff1d430cb17b2d220a17299e0760
Maxim <38497831+StableBig@users.noreply.github.com>
2023-11-27 02:49:44 +0300

    Download books from tululu.org

--- main.py
+++ main.py
@@ -1,15 +1,26 @@
+import os
 import requests
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
-
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
-url = 'https://tululu.org/txt.php?id=32168'
+os.makedirs('books', exist_ok=True)
 
-response = requests.get(url, verify=False)
+for book_id in range(1, 11):
+    url = f'https://tululu.org/txt.php?id={book_id}'
 
-if response.status_code == 200:
-    with open('Пески Марса.txt', 'wb') as file:
-        file.write(response.content)
-    print("Книга 'Пески Марса' успешно скачана!")
-else:
-    print("Ошибка при скачивании книги. Код ответа:", response.status_code)
+    try:
+        response = requests.get(url)
+
+        if response.status_code == 200:
+            # Формируем путь к файлу
+            file_path = os.path.join('books', f'book_{book_id}.txt')
+
+            # Сохраняем содержимое в файл
+            with open(file_path, 'wb') as file:
+                file.write(response.content)
+            print(f"Книга {book_id} успешно скачана!")
+        else:
+            print(f"Ошибка при скачивании книги {book_id}. Код ответа: {response.status_code}")
+
+    except requests.RequestException as e:
+        print(f"Ошибка при скачивании книги {book_id}: {e}")
