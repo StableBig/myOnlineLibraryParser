858c6d424735f1ad485e19a669b8d2e8bc6bd7d7
Maxim <38497831+StableBig@users.noreply.github.com>
2023-11-27 05:00:38 +0300

    Add if main

--- my_bs4_script.py
+++ my_bs4_script.py
@@ -9,7 +9,6 @@ def parse_book_page(html_content):
     """Парсит страницу книги и возвращает данные о книге."""
     soup = BeautifulSoup(html_content, 'html.parser')
 
-    # Название и автор
     title_author_tag = soup.find('h1')
     title, author = None, None
     if title_author_tag:
@@ -17,11 +16,9 @@ def parse_book_page(html_content):
         title = title_author[0].strip()
         author = title_author[1].strip()
 
-    # Жанры
     genre_tags = soup.find('span', class_='d_book').find_all('a')
     genres = [tag.get_text().strip() for tag in genre_tags]
 
-    # Комментарии
     comments = []
     comment_tags = soup.find_all('div', class_='texts')
     for tag in comment_tags:
@@ -29,7 +26,6 @@ def parse_book_page(html_content):
         if comment:
             comments.append(comment.get_text().strip())
 
-    # Сборка всех данных в словарь
     return {
         'title': title,
         'author': author,
@@ -66,30 +62,32 @@ def download_image(url, folder='images/'):
     else:
         raise Exception(f'Ошибка при скачивании изображения: HTTP {response.status_code}')
 
-# Парсинг аргументов командной строки
-parser = argparse.ArgumentParser(description="Скачивание книг с сайта tululu.org")
-parser.add_argument('start_id', type=int, help="ID первой книги для скачивания")
-parser.add_argument('end_id', type=int, help="ID последней книги для скачивания")
-args = parser.parse_args()
+def main(start_id, end_id):
+    for book_id in range(start_id, end_id + 1):
+        try:
+            book_url = f'http://tululu.org/b{book_id}/'
+            response = requests.get(book_url)
+            if response.status_code == 200:
+                book_data = parse_book_page(response.text)
 
-# Основной цикл
-for book_id in range(args.start_id, args.end_id + 1):
-    try:
-        book_url = f'http://tululu.org/b{book_id}/'
-        response = requests.get(book_url)
-        if response.status_code == 200:
-            book_data = parse_book_page(response.text)
+                filename = f"{book_data['title']} - {book_data['author']}"
+                txt_url = f'http://tululu.org/txt.php?id={book_id}'
+                cover_url = f'https://tululu.org/shots/{book_id}.jpg'
 
-            filename = f"{book_data['title']} - {book_data['author']}"
-            txt_url = f'http://tululu.org/txt.php?id={book_id}'
-            cover_url = f'https://tululu.org/shots/{book_id}.jpg'
+                txt_filepath = download_txt(txt_url, filename)
+                img_filepath = download_image(cover_url)
+                print(f"Книга '{book_data['title']}' и её обложка скачаны: {txt_filepath}, {img_filepath}")
+                print(f"Жанры книги: {', '.join(book_data['genres'])}")
+                print(f"Комментарии к книге: {book_data['comments']}")
+            else:
+                print(f"Книга с ID {book_id} не найдена или ошибка в её данных.")
+        except Exception as e:
+            print(f"Ошибка при обработке книги с ID {book_id}: {e}")
 
-            txt_filepath = download_txt(txt_url, filename)
-            img_filepath = download_image(cover_url)
-            print(f"Книга '{book_data['title']}' и её обложка скачаны: {txt_filepath}, {img_filepath}")
-            print(f"Жанры книги: {', '.join(book_data['genres'])}")
-            print(f"Комментарии к книге: {book_data['comments']}")
-        else:
-            print(f"Книга с ID {book_id} не найдена или ошибка в её данных.")
-    except Exception as e:
-        print(f"Ошибка при обработке книги с ID {book_id}: {e}")
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Скачивание книг с сайта tululu.org")
+    parser.add_argument('start_id', type=int, help="ID первой книги для скачивания")
+    parser.add_argument('end_id', type=int, help="ID последней книги для скачивания")
+    args = parser.parse_args()
+
+    main(args.start_id, args.end_id)
