972bf4ce841fbfaa5455d019c3457af54751a30a
Maxim L. <38497831+StableBig@users.noreply.github.com>
2024-12-07 23:39:30 +0100

    Add --dest_folder, --skip_imgs, --skip_txt for more control over downloads

--- parse_tululu_category.py
+++ parse_tululu_category.py
@@ -42,10 +42,11 @@ def parse_book_page(html_content, base_url):
     }
 
 
-def download_txt(book_id, filename, folder='books/'):
+def download_txt(book_id, filename, folder):
     safe_filename = sanitize_filename(f'{filename}.txt')
-    os.makedirs(folder, exist_ok=True)
-    file_path = os.path.join(folder, safe_filename)
+    folder_path = os.path.join(folder, 'books')
+    os.makedirs(folder_path, exist_ok=True)
+    file_path = os.path.join(folder_path, safe_filename)
 
     params = {'id': book_id}
     response = requests.get('http://tululu.org/txt.php', params=params)
@@ -57,9 +58,10 @@ def download_txt(book_id, filename, folder='books/'):
     return file_path
 
 
-def download_image(url, folder='images/'):
-    os.makedirs(folder, exist_ok=True)
-    file_path = os.path.join(folder, os.path.basename(url))
+def download_image(url, folder):
+    folder_path = os.path.join(folder, 'images')
+    os.makedirs(folder_path, exist_ok=True)
+    file_path = os.path.join(folder_path, os.path.basename(url))
 
     response = requests.get(url)
     check_for_redirect(response)
@@ -94,13 +96,14 @@ def main():
     )
     parser.add_argument('--start_page', type=int, default=1, help='С какой страницы начинать скачивание книг')
     parser.add_argument('--end_page', type=int, default=1, help='На какой странице остановить скачивание книг')
+    parser.add_argument('--dest_folder', type=str, default='.', help='Путь к каталогу для сохранения файлов')
+    parser.add_argument('--skip_imgs', action='store_true', help='Не скачивать обложки книг')
+    parser.add_argument('--skip_txt', action='store_true', help='Не скачивать текстовые файлы книг')
     args = parser.parse_args()
 
-    start_page = args.start_page
-    end_page = args.end_page
-
     base_category_url = 'http://tululu.org/l55/'
-    book_links = get_all_book_links_from_all_pages(base_category_url, start_page=start_page, end_page=end_page)
+    book_links = get_all_book_links_from_all_pages(base_category_url, start_page=args.start_page,
+                                                   end_page=args.end_page)
 
     book_metadata = []
     for book_url in book_links:
@@ -113,15 +116,18 @@ def main():
             book_id = book_url.split('/')[-2].replace('b', '')
             filename = f"{book_details['title']} - {book_details['author']}"
 
-            try:
-                txt_filepath = download_txt(book_id, filename)
-            except requests.exceptions.RequestException as e:
-                print(f"Ошибка при скачивании текста для книги {filename}: {e}", file=sys.stderr)
-                continue
-
-            if book_details['cover_url']:
+            if not args.skip_txt:
                 try:
-                    img_filepath = download_image(book_details['cover_url'])
+                    txt_filepath = download_txt(book_id, filename, args.dest_folder)
+                except requests.exceptions.RequestException as e:
+                    print(f"Ошибка при скачивании текста для книги {filename}: {e}", file=sys.stderr)
+                    continue
+            else:
+                txt_filepath = None
+
+            if book_details['cover_url'] and not args.skip_imgs:
+                try:
+                    img_filepath = download_image(book_details['cover_url'], args.dest_folder)
                 except requests.exceptions.RequestException as e:
                     print(f"Ошибка при скачивании обложки для книги {filename}: {e}", file=sys.stderr)
             else:
@@ -131,11 +137,11 @@ def main():
             book_details['img_path'] = img_filepath
             book_metadata.append(book_details)
 
-            print(f"Книга '{book_details['title']}' скачана. Ссылка: {book_url}")
+            print(f"Книга '{book_details['title']}' скачана.")
         except requests.RequestException as e:
             print(f"Ошибка при обработке книги {book_url}: {e}", file=sys.stderr)
 
-    with open('books.json', 'w', encoding='utf-8') as file:
+    with open(os.path.join(args.dest_folder, 'books.json'), 'w', encoding='utf-8') as file:
         json.dump(book_metadata, file, ensure_ascii=False, indent=4)
     print(f"Информация о книгах сохранена в books.json")
 
